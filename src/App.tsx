fa030ed0965c870c3930cac36a255b43df4be529
24siefil <24siefil@gmail.com>
2022-06-18 00:04:21 +0900

    feat: pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/common/Layout';
-import Home from './pages/Home';
-import My from './pages/My';
-import Settings from './pages/Settings';
+
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import NotFound from './pages/NotFound';
+// import Home from './pages/Home';
+// import My from './pages/My';
+// import Settings from './pages/Settings';
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/my" element={<My />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route index element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
